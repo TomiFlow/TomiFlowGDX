7f5a0233d1211e26926a46a0c0e90c3b30640c10
Rafael García <themrrafus@gmail.com>
2017-12-01 11:23:30 +0100

    Initial commit

---
+++ desktop/src/com/tomiflow/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tomiflow.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tomiflow.game.Principal;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Principal(), config);
+	}
+}
